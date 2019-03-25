b4a769e1ce1fbbdcd7da1fe48414341ccb4c50cb
alixcaroline <alixcaroline13@gmail.com>
2019-03-25 11:55:42 +0100

    create base structure

---
+++ Routes.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::set('index.php', function(){
+  Index::CreateView('index');
+});
+
+Route::set('register.php', function(){
+  Register::CreateView('register');
+});
+
+Route::set('login.php', function(){
+  Login::CreateView('login');
+});
+
+?>
