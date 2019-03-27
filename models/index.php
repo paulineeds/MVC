6b9644c84dc37a4b72b7debc653e953fdc0407c7
dominicho97 <dominic.ho97@gmail.com>
2019-03-27 10:53:12 +0100

    added index.php/model

---
+++ models/index.php
@@ -0,0 +1,25 @@
+
+
+
+
+
+<?php
+include('./classes/DB.php');
+function isLoggedIn() {
+
+  //checks if the token can be found in DB/is valid
+        if (isset($_COOKIE['SNID'])) {
+                if (DB::query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($_COOKIE['SNID'])))) {
+                        $userid = DB::query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($_COOKIE['SNID'])))[0]['user_id'];
+
+                        return true;
+                }
+              }
+
+              return false;
+            }
+            if (isLoggedIn()){
+              echo 'Logged in';
+          } else {
+            echo 'Not logged in';
+          }
