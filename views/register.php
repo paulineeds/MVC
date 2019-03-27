70edcca0e453ebde84cf99b5451d281053a4f43d
Pauline De Smet <paulinedesmet@me.com>
2019-03-27 11:00:14 +0100

    fix it

--- views/register.php
+++ views/register.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Register</title>
-</head>
-<body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="/../css/style.css">
 
-<?php
-  require_once('./models/register.php');
-?>
+    
+  </head>
+  <body>
 
-  <form method="POST" action="register.php">
-  <div class="container">
-    <h1>Register</h1>
-    <p>Please fill in this form to create an account.</p>
+    <?php
+    require_once('./models/register.php');
+    ?>
 
-    <input type="text" name="username" value="" placeholder="Username ..."><br>
-<input type="password" name="password" value="" placeholder="Password ..."><br>
-<input type="email" name="email" value="" placeholder="someone@somesite.com"><br>
-<input type="submit" name="createaccount" value="Create Account">
-</form>
+    <form method="POST" action="register.php">
+      <div class="container">
+        <h1>Register</h1>
+        <p>Please fill in this form to create an account.</p>
 
-  <div class="container login">
-    <p>Already have an account? <a href="login.php">Log in</a>.</p>
-  </div>
-</body>
+        <input type="text" name="username" value="" placeholder="Username ..."><br>
+      <input type="password" name="password" value="" placeholder="Password ..."><br>
+      <input type="email" name="email" value="" placeholder="someone@somesite.com"><br>
+      <input type="submit" name="createaccount" value="Create Account">
+    </form>
+
+    <div class="container login">
+      <p>Already have an account? <a href="login.php">Log in</a>.</p>
+    </div>
+  </body>
 </html>
