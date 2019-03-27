4004cb4f81f2108f41f24e539e34454998026985
dominicho97 <dominic.ho97@gmail.com>
2019-03-27 11:58:33 +0100

    added folder css

--- views/register.php
+++ views/register.php
@@ -1,25 +1,28 @@
+<?php
+  require_once('../models/register.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
   <title>Register</title>
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
 
-<?php
-  require_once('./models/register.php');
-?>
+
 
   <form method="POST" action="register.php">
   <div class="container">
     <h1>Register</h1>
     <p>Please fill in this form to create an account.</p>
 
-    <input type="text" name="username" value="" placeholder="Username ..."><p />
-<input type="password" name="password" value="" placeholder="Password ..."><p />
-<input type="email" name="email" value="" placeholder="someone@somesite.com"><p />
+    <input type="text" name="username" value="" placeholder="Username ..."><br>
+<input type="password" name="password" value="" placeholder="Password ..."><br>
+<input type="email" name="email" value="" placeholder="someone@somesite.com"><br>
 <input type="submit" name="createaccount" value="Create Account">
 </form>
 
@@ -28,3 +31,4 @@
   </div>
 </body>
 </html>
+html>
