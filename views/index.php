1c889c3058dd4167979ca37c595b1d4fe543a891
Pauline De Smet <paulinedesmet@me.com>
2019-03-28 12:07:44 +0100

    first startscherm css + correcties

--- views/index.php
+++ views/index.php
@@ -4,11 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
   <title>Welcome</title>
 </head>
-<body>
-  <h1>Welcome</h1>
-  <a href ="register.php">Register</a>
-  <a href ="login.php">Login</a>
-</body>
+  <body>
+    <h1>Somename</h1>
+    <div class="flex">
+      <div class="containerwelkom">
+      <h1>Welcome</h1>
+      <p class="text">Log in or join us now!</p>
+      <form>
+        <input class="btnleft" id="fixbutton" type="button" value="Join" onclick="window.location.href='register.php'" />
+        <input class="btnright" type="button" value="Login" onclick="window.location.href='login.php'" />
+      </form>
+     </div>
+    </div>
+  </body>
 </html>
--- views/login.php
+++ views/login.php
@@ -9,6 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <title>Log in</title>
 </head>
