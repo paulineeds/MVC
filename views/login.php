27f3513df91635746a873bfd644e6f5a85501fd3
Pauline De Smet <paulinedesmet@me.com>
2019-03-28 09:39:17 +0100

    first login design

--- views/login.php
+++ views/login.php
@@ -9,15 +9,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" type="text/css" href="../css/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <title>Log in</title>
 </head>
 <body>
-<h1>Login to your account</h1>
-<form action="login.php" method="post">
-<input type="text" name="username" value="" placeholder="Username ..."><br>
-<input type="password" name="password" value="" placeholder="Password ..."><br>
-<input type="submit" name="login" value="Login">
+<h1>Somename</h1>
+<div class="flex">
+<div class="logincontainer">
+<h1>Login </h1>
+<i class="fas fa-unlock-alt"></i>
+<form action="login.php" method="post" class="loginform">
+<input type="text" name="username" value="" placeholder="Username"><br>
+<input type="password" name="password" value="" placeholder="Password"><br>
+<input type="submit" class="button" name="login" value="Login">
 </form>
+</div>
 </div>
 </body>
 </html>
