715aad2a9eda924e97f21d3d965b69176a6d33ef
Pauline De Smet <paulinedesmet@me.com>
2019-03-28 16:26:14 +0100

    logout

--- views/logout.php
+++ views/logout.php
@@ -8,15 +8,23 @@ require_once('../models/logout.php');
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Log out</title>
+  <link rel="stylesheet" type="text/css" href="../css/logout.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <title>Logout</title>
 </head>
-<body>
-  <h1>Log out</h1>
-  <p>Are you sure you'd like to log out?</p>
-  <form action="logout.php" method="post">
-    <input type="checkbox" name="alldevices" value="alldevices"> Log out all devices <br>
-    <input type="submit" name="confirm" value="Confirm">
-  
-  </form>
-</body>
+  <body>
+    <h1>Somename</h1>
+    <div class="flex">
+      <div class="containerlogout">
+      <h1>Logout</h1>
+      <i class="fas fa-lock"></i>
+      <p>Are you sure you'd like to log out?</p>
+      <form action="logout.php" method="post">
+        <input class="checkbox" type="checkbox" name="alldevices" value="alldevices"> Log out all devices <br>
+        <input class="button" type="submit" name="confirm" value="Confirm">
+      </form>
+    </div>
+    </div>
+  </body>
 </html>
