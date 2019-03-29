86d51264e3a8113f1d3f3d684c50a43cc32c3ab4
dominicho97 <dominic.ho97@gmail.com>
2019-03-29 14:58:17 +0100

    updated profile.php

--- models/profile.php
+++ models/profile.php
@@ -1,44 +1,5 @@
 
-<?php
-/*
-$username = "";
-$verified = False;
-$isFollowing = False;
 
-if (isset($_GET['username'])) {
-        if (DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))) {
-                $username = DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['username'];
-                $userid = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['id'];
-                $verified = DB::query('SELECT verified FROM users WHERE username=:username', array(':username'=>$_GET['username']))[0]['verified'];
-
-                $followerid = Login::isLoggedIn();
-                if (isset($_POST['follow'])) {
-                        if ($userid != $followerid) {
-                                if (!DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
-                                        DB::query('INSERT INTO followers VALUES (:id, :userid, :followerid)', array(':id'=>null, ':userid'=>$userid, ':followerid'=>$followerid));
-                                } else {
-                                        echo 'Already following!';
-                                }
-                                $isFollowing = True;
-                        }
-                }
-                if (isset($_POST['unfollow'])) {
-                        if ($userid != $followerid) {
-                                if (DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
-                                        DB::query('DELETE FROM followers WHERE user_id=:userid AND follower_id=:followerid', array(':userid'=>$userid, ':followerid'=>$followerid));
-                                }
-                                $isFollowing = False;
-                        }
-                }
-                if (DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid'=>$userid))) {
-                        echo 'Already following!';
-                        $isFollowing = True;
-                }
-        } else {
-                die('User not found!');
-        }
-}*/
-?>
 <?php
 
 require_once('../classes/Login.php');
